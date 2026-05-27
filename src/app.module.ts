b0fe246537fbf164683604ab573134157fa79795
Andrii Serputko <aserputko@gmail.com>
2026-05-27 12:07:48 +0300

    feat: init project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
