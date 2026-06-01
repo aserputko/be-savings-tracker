35dc0de9abe239a8ce89c9a3b39f01a752a6db7c
Andrii Serputko <aserputko@gmail.com>
2026-06-01 23:31:24 +0300

    refactor: remove AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { WinstonModule } from 'nest-winston';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { winstonConfig } from './logger/logger.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { SavingsGoalModule } from './modules/savings-goal/savings-goal.module';
@@ -16,7 +14,5 @@ import { PrismaModule } from './prisma/prisma.module';
     AuthModule,
     SavingsGoalModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
